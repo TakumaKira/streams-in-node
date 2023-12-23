f269f90eded9b820044630ad3b572b113bbea9d4
Takuma Kira <takuma@kirakiraworx.com>
2023-12-23 20:20:45 +0900

    Use stream.pipeline

--- copyFileStreaming.js
+++ copyFileStreaming.js
@@ -1,12 +1,11 @@
 import fs from "node:fs";
+import stream from "node:stream/promises";
 
-function copyFile() {
+async function copyFile() {
+  const readStream = fs.createReadStream("myFile.txt");
   const writeStream = fs.createWriteStream("myCopiedFile.txt");
-  const readStream = fs.createReadStream("myFile.txt").pipe(writeStream);
 
-  readStream.on("finish", () => {
-    writeStream.end();
-  });
+  await stream.pipeline(readStream, writeStream);
 }
 
 copyFile();
