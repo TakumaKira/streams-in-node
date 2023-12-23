03fcde14dbb2ce193ccc56f067aff370d8f50cb3
Takuma Kira <takuma@kirakiraworx.com>
2023-12-23 20:33:59 +0900

    Handling Backpressure

--- 04-2-writeCsvWithStream.js
+++ 04-2-writeCsvWithStream.js
@@ -2,16 +2,35 @@ import fs from "node:fs";
 import logMemory from "./logMemory.js";
 
 async function writeCsvWithStream() {
-  const writeStream = fs.createWriteStream("myFile.csv");
+  const writeStream = fs
+    .createWriteStream("myFile.csv")
+    .on("error", (err) => console.error("Error writing to stream:", err))
+    .on("drain", writeData);
+
   writeStream.write("Number,Squared\n");
 
-  for (let i = 0; i < 10_000_000; i++) {
-    writeStream.write(`${i},${i * i}\n`);
+  let current = 0;
+
+  function writeData() {
+    const max = 10_000_000;
+
+    while (current < max) {
+      const result = writeStream.write(`${current},${current * current}\n`);
+      current++;
+
+      if (!result) {
+        break;
+      }
+
+      if (current >= max) {
+        logMemory();
+        writeStream.end();
+        break;
+      }
+    }
   }
 
-  logMemory();
-
-  writeStream.end();
+  writeData();
 }
 
-await writeCsvWithStream();
+writeCsvWithStream();
